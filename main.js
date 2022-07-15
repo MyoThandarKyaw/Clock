49aa84a203ef531c943fc8292f734f2d51f4be5c
Myo Thandar Kyaw <myothandarkyaw235@gmail.com>
2022-07-15 19:37:48 +0630

    update

---
+++ main.js
@@ -0,0 +1,23 @@
+
+setInterval(setClock, 1000);
+
+let hourHand = document.querySelector('[data-hour-hand]');
+let minuteHand = document.querySelector('[data-minute-hand]');
+let secondHand = document.querySelector('[data-second-hand]');
+
+
+
+function setClock() {
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds() / 60;
+    const minuteRatio = (secondRatio +currentDate.getMinutes()) / 60;
+    const hourRatio = (minuteRatio +currentDate.getHours()) / 12;
+
+    setRotation(hourHand, hourRatio)
+    setRotation(minuteHand, minuteRatio)
+    setRotation(secondHand, secondRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty("--rotation", rotationRatio * 360)
+}
